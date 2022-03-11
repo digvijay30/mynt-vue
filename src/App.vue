aa49a26a0fa6e2511af386684491093e05c45508
Digvijay30 <digitalutilization@gmail.com>
2022-03-11 10:52:32 +0530

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
 <template>
-  <div id="app">
-   <h1>MYNT</h1>
+  <div class="container">
+     <AppHeader/>
   </div>
 </template>
 
 <script>
-
-
+import AppHeader from './components/AppHeader';
 export default {
   name: 'App',
+  components:{
+    AppHeader
+  }
 }
 </script>
 
+<style>
+
+:root
+{
+  --sizing-padding:0 7rem 0 7rem;
+  --dark-black:#343a40;
+  --gray:gray;
+  --bg-color:gray;
+  --primary-font-color:white;
+  --secondary-font-color:gray;
+
+  --flex-primary-gap:0.5rem;
+  --flex-secondary-gap:1rem;
+  --flex-large-gap:1.5rem;
+
+  --font-size-h3:14px;
+  --font-size-h2:20px;
+  --font-size-h4:12px;
+
+}
+
+*{
+  padding: 0;
+  margin: 0;
+  font-family:Arial, Helvetica, sans-serif;
+}
+
+/* flex common css class */
+
+.flex {
+  display: flex;
+}
+
+.flex-center
+{
+   justify-content: center;
+   align-items: center;
+}
+.justify-center {
+  justify-content: center;
+}
+.align-items {
+  align-items: center;
+}
+
+.sizing {
+  justify-content: space-between;
+}
+
+</style>
 
