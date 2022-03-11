220bd9aef359c7afab4be56f08b68f06ef9a0038
Digvijay30 <digitalutilization@gmail.com>
2022-03-11 10:43:38 +0530

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+     <AppHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+:root
+{
+  --sizing-padding:0 7rem 0 7rem;
+  --dark-black:#343a40;
+  --gray:gray;
+  --bg-color:gray;
+  --primary-font-color:white;
+  --secondary-font-color:gray;
+
+  --flex-primary-gap:0.5rem;
+  --flex-secondary-gap:1rem;
+  --flex-large-gap:1.5rem;
+
+  --font-size-h3:14px;
+  --font-size-h2:20px;
+  --font-size-h4:12px;
+
 }
+
+*{
+  padding: 0;
+  margin: 0;
+  font-family:Arial, Helvetica, sans-serif;
+}
+
+/* flex common css class */
+
+.flex {
+  display: flex;
+}
+
+.flex-center
+{
+   justify-content: center;
+   align-items: center;
+}
+.justify-center {
+  justify-content: center;
+}
+.align-items {
+  align-items: center;
+}
+
+.sizing {
+  justify-content: space-between;
+}
+
 </style>
+
